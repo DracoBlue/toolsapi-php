caa803f9c3f0e2d7cef541d0e0d9c6de3dff1ff3
DracoBlue <JanS@DracoBlue.de>
2013-06-09 17:32:02 +0200

    removed deprecated comment

--- tests/test_stdin_with_jsl.php
+++ tests/test_stdin_with_jsl.php
@@ -1,7 +1,5 @@
 <?php
 
-// $app->run('fontcustom', array('file' => dirname(__FILE__) . '/fixtures/yes.svg', 'file2' => dirname(__FILE__) . '/fixtures/no.svg'));
-
 $tool = $app->createTool('jsl');
 $tool->addArgument('-stdin');
 $tool->pipeLocalFileToStdIn(dirname(__FILE__) . '/fixtures/invalid_js_file.txt');
