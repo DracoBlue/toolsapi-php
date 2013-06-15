1f1fd8f907ee5c5bbf4c49f01365b0e2c28497c3
DracoBlue <JanS@DracoBlue.de>
2013-06-15 14:43:55 +0200

    Added possibility to create instance from toolsapi.properties file

--- src/ToolsApi.php
+++ src/ToolsApi.php
@@ -10,6 +10,41 @@ class ToolsApi
         $this->client->getDefaultHeaders()->set('Accept', 'application/hal+json');
         $this->client->getDefaultHeaders()->set('Authorization', 'Basic ' . base64_encode($username . ':' . $password));
     }
+
+    public static function createInstance()
+    {
+        $properties_file_paths = array(
+            getcwd() . '/toolsapi.properties',
+            '~/toolsapi.properties',
+            '/etc/toolsapi.properties',
+        );
+
+        foreach ($properties_file_paths as $properties_file_path)
+        {
+            if (file_exists($properties_file_path))
+            {
+                $properties_content = file_get_contents($properties_file_path);
+                preg_match_all('/^([^\=]+)\=(.*)$/m', $properties_content, $properties_matches);
+
+                unset($properties_matches[0]);
+
+                $properties = array();
+                foreach ($properties_matches[1] as $pos => $properties_match)
+                {
+                    $properties[$properties_match] = $properties_matches[2][$pos];
+                }
+
+                if (!isset($properties['user']) || !isset($properties['password']) || !isset($properties['url']))
+                {
+                    throw new Exception('toolsapi.properties must contain user, password and url key!');
+                }
+
+                return new self($properties['url'], $properties['user'], $properties['password']);
+            }
+        }
+
+        throw new \Exception("Cannot find toolsapi.properties at any of those paths: " . implode(', ', $properties_file_paths));
+    }
     
     public function createTool($name)
     {
