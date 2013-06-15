5cb46466f817574ecfecc4023dcbce2aba49c35c
DracoBlue <JanS@DracoBlue.de>
2013-06-15 18:21:52 +0200

    Initialize the tests user with the toolsapi.properties account

--- _before_test.php
+++ _before_test.php
@@ -5,6 +5,7 @@ if (file_exists(dirname(__FILE__)) . '/vendor/autoload.php')
     require_once(dirname(__FILE__) . '/vendor/autoload.php');
 }
 
-$app = new ToolsApi('https://ssl.erstmal.com/toolsapi/', "tester", "password");
-
-
+#$app = new ToolsApi('https://ssl.erstmal.com/toolsapi/', "tester", "password");
+#$app = new ToolsApi('http://toolsapi.local/', "tester", "password");
+#$app = new ToolsApi('http://toolsapi.com/', "tester", "password");
+$app = ToolsApi::createInstance();
