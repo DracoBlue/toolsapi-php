8837e678313b5def0a73d6ec348e9e58a02406b7
DracoBlue <JanS@DracoBlue.de>
2013-07-05 23:00:26 +0200

    Removed jsl test

--- tests/test_stdin_with_jsl.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$tool = $app->createTool('jsl');
-$tool->addArgument('-stdin');
-$tool->pipeLocalFileToStdIn(dirname(__FILE__) . '/fixtures/invalid_js_file.txt');
-$response = $tool->execute();
-assert(strpos($response, ' missing } after function body') > 0);
