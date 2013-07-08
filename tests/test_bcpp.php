0f8aa9ebad6c25981a71bf98f69b8b5b3a82b555
DracoBlue <JanS@DracoBlue.de>
2013-07-08 17:29:42 +0200

    Hide stderr for bcpp

--- tests/test_bcpp.php
+++ tests/test_bcpp.php
@@ -5,6 +5,7 @@ $tool->addArgument('-fnc');
 $tool->addLocalFile(dirname(__FILE__) . '/fixtures/bcpp.cfg');
 $tool->addArgument('-fi');
 $tool->addLocalFile(dirname(__FILE__) . '/fixtures/test_c_file.txt');
+$tool->pipeStdErrToLocalFile(dirname(__FILE__) . '/bcpp-stderr.txt');
 $real_content = file_get_contents(dirname(__FILE__) . '/fixtures/test_c_file.txt');
 $response = $tool->execute();
 
